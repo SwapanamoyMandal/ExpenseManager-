411f9c4dc2bad419f04bf0b1358dbdfb9404ee61
Swapnamoy Mandal <121283380+SwapanamoyMandal@users.noreply.github.com>
2025-07-24 00:20:50 +0530

    Update Navbar.jsx

--- manageExpense/src/components/Navbar.jsx
+++ manageExpense/src/components/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from 'react';
+import fullLogo from '../assets/expanzzpng.png';
+import expenzzLogo from '../assets/expenzzLOGO.png';
+import expenzzTxt from '../assets/expenzzTXT.png';
 
 const Navbar = () => {
   return (
-    <div className='bg-primary w-full flex justify-center sm:justify-between items-center p-1 sm:px-15 sm:py-2 '>
-            <img src='src\assets\expanzzpng.png' alt='full' className='w-50 sm:hidden'/>
-            <div>
-                <img src="src\assets\expenzzLOGO.png" alt="logo" className='w-25 hidden sm:inline-block' />
-            </div>
-            <div>
-                <img src='src/assets/expenzzTXT.png' alt='txt' className='w-70 hidden sm:inline-block' />
-            </div>
+    <div className='bg-primary w-full flex justify-center sm:justify-between items-center p-1 sm:px-15 sm:py-2'>
+      <img src={fullLogo} alt='full' className='w-50 sm:hidden' />
+      <div>
+        <img src={expenzzLogo} alt='logo' className='w-25 hidden sm:inline-block' />
+      </div>
+      <div>
+        <img src={expenzzTxt} alt='txt' className='w-70 hidden sm:inline-block' />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
