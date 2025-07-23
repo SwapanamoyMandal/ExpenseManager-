287a981df8aaabfa730d3a9467a5b468f1bd6297
Swapnamoy Mandal <121283380+SwapanamoyMandal@users.noreply.github.com>
2025-07-24 00:37:26 +0530

    Update database.js

--- manageExpenseServer/database.js
+++ manageExpenseServer/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/ExpenseDatabase', {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
 
